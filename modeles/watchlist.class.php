6390ddce01816e1124a5db48a0b5bf17d3afe745
Marylou Lohier <mlohier001@iutbayonne.univ-pau.fr>
2024-11-06 14:59:37 +0100

    Mise en place de la classe WatchList

--- modeles/watchlist.class.php
+++ modeles/watchlist.class.php
@@ -2,15 +2,53 @@
 
 class Watchlist{
     // Attributs 
+    private int $id; //Identifiant de la watchlist
+    private string $nom; //Nom de la watchlist
+    private string $desc; //Description de la watchlist
+    private bool $estPublique; //Indicateur de publicité d'une WL
+    private $date; //Date de création de la WL
 
     // Constructeur
-
+    public function __construct(int $id, string $nom, string $desc, bool $estPublique, $date){
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->desc = $desc;
+        $this->estPublique = $estPublique;
+        $this->date = $date;
+    }
 
     // Encapsulation
     // Getters
+    public function getId(): int{
+        return $this->id;
+    }
+    public function getNom(): string{
+        return $this->nom;
+    }
+    public function getDesc(): string{
+        return $this->desc;
+    }
+    public function getPublicite(): bool{
+        return $this->estPublique;
+    }
+    public function getDate(){
+        return $this->date;
+    }
 
     // Setters
-
-
-    // Méthodes 
+    public function setId(int $id) : void{
+        $this->id = $id;
+    }
+    public function setNom(string $nom) : void{
+        $this->nom = $nom;
+    }
+    public function setDesc(string $desc) : void{
+        $this->desc = $desc;
+    }
+    public function setPublicite(bool $estPublique) : void{
+        $this->estPublique = $estPublique;
+    }
+    public function setDate($date) : void{
+        $this->date = $date;
+    }
 }
