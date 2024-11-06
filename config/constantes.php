e53c319fe7e25ffa82ddaa0b1b55f26853c0067e
Sylvain Trouilh <strouilh@iutbayonne.univ-pau.fr>
2024-11-06 14:48:36 +0100

    mise en place des du modele bd

---
+++ config/constantes.php
@@ -0,0 +1,10 @@
+<?php
+
+//Connexion à la base de données
+define('DB_HOST', 'lakartxela.iutbayonne.univ-pau.fr');
+define('DB_NAME', 'mbourciez_pro');
+define('DB_USER', 'mbourciez_pro');
+define('DB_PASS', 'mbourciez_pro');
+
+define('DB_PREFIX', 'vhs_');
+
