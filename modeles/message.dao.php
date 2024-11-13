83805e9475f8f2409d051649421b04194d31e80a
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2024-11-13 16:47:05 +0100

    Maj des commentaires de messages

--- modeles/message.dao.php
+++ modeles/message.dao.php
@@ -1,30 +1,65 @@
 <?php 
-
-// Classe MessageDAO
+/**
+ * @file message.dao.php
+ * 
+ * @brief Classe MessageDAO
+ * 
+ * @details Classe permettant de gérer les accès à la base de données pour les messages
+ * 
+ * @date 12/11/2024
+ * 
+ * @version 1.0
+ * 
+ * @author Maxime Bourciez <maxime.bourciez@gmail.com>
+ */
 class MessageDAO{
     // Attiributs 
-    private ?PDO $pdo; // Connexion à la BD
+    /**
+     * @var PDO|null $pdo Connexion à la BD
+     */
+    private ?PDO $pdo;
 
     // Constructeur
+    /**
+     * @brief Constructeur de la classe MessageDAO
+     * 
+     * @param PDO|null $pdo Connexion à la base de données
+     */
     public function __construct(?PDO $pdo = null){
         $this->pdo = $pdo;
     }
 
     // Encapsulation
-    // Getters
+    // Getter
+    /**
+     * @brief Getter de la connexion à la base de données
+     *
+     * @return PDO|null Connexion à la base de données
+     */
     public function getPdo(): ?PDO{
         return $this->pdo;
     }
 
-    // Setters
+    // Setter
+    /**
+     * @brief Setter de la connexion à la base de données
+     * 
+     * @param PDO $pdo Connexion à la base de données
+     * @return self
+     */
     public function setPdo(PDO $pdo): self{
         $this->pdo = $pdo;
         return $this;
     }
 
     // Méthodes
-
     // Méthodes d'hydratation
+    /**
+     * @brief Méthode d'hydratation d'un message
+     * 
+     * @param array $row Ligne de la base de données
+     * @return Message Objet Message hydraté
+     */
     public function hydrate(array $row): Message{ 
         // Récupération de l'utilisateur
         $user = new Utilisateur();
@@ -42,9 +77,15 @@ class MessageDAO{
         $id_fil = $row['idFil'];
 
         // Retourner le message
-        return new Message($id, $valeur, $nbLike, $nbDislike, $date, $user, $id_message_parent, $fil);
+        return new Message($id, $valeur, $nbLike, $nbDislike, $date, $user, $id_message_parent, $id_fil);
     } 
 
+    /**
+     * @brief Méthode d'hydratation de tous les messages
+     * 
+     * @param array $rows Tableau de lignes de la base de données
+     * @return array<Message> Tableau d'objets Message hydratés
+     */
     function hydrateAll(array $rows): array{
         $messages = [];
         foreach($rows as $row){
@@ -56,7 +97,12 @@ class MessageDAO{
 
 
     // Méthodes de recherche
-    // Lister tous les messages
+    /**
+     * @brief Méthode permettant de lister tous les messages
+     * 
+     * @return array<Message> Tableau d'objets Message
+     * 
+     */
     public function listerMessages(): array{
         $sql = "SELECT * FROM" . DB_PREFIX . "message";
         $stmt = $this->pdo->query($sql);
@@ -64,7 +110,14 @@ class MessageDAO{
         return $this->hydrateAll($stmt->fetchAll());
     }
 
-    // Chercher un message par son id
+    /**
+     * @brief Méthode permettant de chercher un message par son id
+     *
+     * @note useless pour le moment
+     *
+     * @param integer $id
+     * @return Message|null
+     */
     public function chercherMessageParId(int $id): ?Message{
         $sql = "SELECT * FROM" . DB_PREFIX . "message WHERE id = :id";
         $stmt = $this->pdo->prepare($sql);
@@ -76,7 +129,14 @@ class MessageDAO{
 
 
     
-    // Lister tous les messages d'un utilisateur par son id 
+    /**
+     * @brief Méthode permettant de lister les messages d'un utilisateur par id_user
+     *
+     * @details Méthode permettant de lister les messages d'un utilisateur par son id pour les lui afficher sur son profil
+     * 
+     * @param integer $id_user
+     * @return array
+     */
     public function listerMessagesParIdUser(int $id_user): array{
         $sql = "SELECT * FROM" . DB_PREFIX . "message WHERE id_user = :id_user";
         $stmt = $this->pdo->prepare($sql);
@@ -86,17 +146,14 @@ class MessageDAO{
         return $stmt->fetchAll();
     }
 
-    // Récupérer les informations d'un message par son id
-    public function getMessageParId(int $id): ?Message{
-        $sql = "SELECT * FROM" . DB_PREFIX . "message WHERE id = :id";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Message');
-        return $stmt->fetch();
-    }
-
-    // Ajouter un message 
+    /**
+     * @brief Méthode d'ajout de message
+     * 
+     * @details Méthode permettant d'ajouter un message à la base de données à partir d'un objet Message passé en paramètre.
+     *
+     * @param Message $message
+     * @return boolean
+     */
     public function ajouterMessage(Message $message): bool{
         // Préparation de la requête
         $sql = "INSERT INTO" . DB_PREFIX . "message (valeur, nbLike, nbDislike, date, id_user, id_message_parent, id_fil) VALUES (:valeur, :nbLike, :nbDislike, :date, :id_user, :id_message_parent, :id_fil)";
