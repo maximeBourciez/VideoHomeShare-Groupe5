557f6572d67ed9b1ca2d053e6b35ea2164f03098
Andoni Berho <aberho001@iutbayonne.univ-pau.fr>
2024-12-16 10:13:21 +0100

    continuation du controller et creation de la methode accueil

--- controller/controller_watchlist.class.php
+++ controller/controller_watchlist.class.php
@@ -1,7 +1,24 @@
 <?php
 
 
-class ControllerWatchlist extends Controller
+class ControllerWhatchlist extends Controller
 {
+    public function __construct(\Twig\Environment $twig, \Twig\Loader\FilesystemLoader $loader)
+    {
+        parent::__construct($twig, $loader);
+    }
+
+    /**
+     * @bref permet d'afficher la page d'accueil des watchlists
+     *
+     * @return void
+     */
+    public function accueil(): void
+    {
+
+        //Génération de la vue
+        $template = $this->getTwig()->load('whatchlists.html.twig');
+        echo $template->render(array());
+    }
 
 }
