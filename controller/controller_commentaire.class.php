833935d7463f46a35c1ce70195f64db458c61b14
François Barlic <francois.barlic@etud.univ-pau.fr>
2025-01-11 14:18:54 +0100

    Factorisation de createCommentaire ajout de style dans le modal et message d'erreur

--- controller/controller_commentaire.class.php
+++ controller/controller_commentaire.class.php
@@ -3,125 +3,74 @@
 /**
  * @brief Contrôleur gérant les commentaires
  * 
- * Cette classe gère la création et la gestion des commentaires sur les contenus
+ * Cette classe gère la création et la gestion des commentaires
  * 
  * @author François Barlic <<francois.barlic57@gmail.com>>
  * @version 1.0
  */
 class ControllerCommentaire extends Controller {
-    /**
-     * @brief Constructeur du contrôleur de commentaires
-     * 
-     * @param \Twig\Environment $twig L'environnement Twig
-     * @param \Twig\Loader\FilesystemLoader $loader Le chargeur de fichiers Twig
-     */
+    private const TYPE_CONTENU = 'contenu';
+    private const TYPE_COLLECTION = 'collection';
+    
     public function __construct(\Twig\Environment $twig, \Twig\Loader\FilesystemLoader $loader) {
         parent::__construct($twig, $loader);
     }
 
     /**
+     * @brief Point d'entrée pour la création de commentaire
+     */
+    public function createCommentaire(): void {
+        // Détermine le type de commentaire basé sur les paramètres POST
+        $type = isset($_POST['idContenu']) ? self::TYPE_CONTENU : 
+               (isset($_POST['idCollection']) ? self::TYPE_COLLECTION : null);
+        
+        if ($type === null) {
+            throw new Exception("Type de commentaire non spécifié");
+        }
+
+        $this->handleCommentaire($type);
+    }
+
+    /**
      * @brief Crée un nouveau commentaire pour un contenu
-     * 
-     * Cette méthode traite la soumission d'un nouveau commentaire :
-     * - Récupère les données du formulaire
-     * - Crée un nouvel objet Commentaire
-     * - Sauvegarde le commentaire dans la base de données
-     * - Redirige vers la page du contenu
-     * 
-     * @return void
      */
     public function createCommentaireContenu(): void {
-        // Vérification de la session et désérialisation de l'utilisateur
-        $utilisateur = isset($_SESSION['utilisateur']) ? unserialize($_SESSION['utilisateur']) : null;
-        
-        if (!$utilisateur) {
-            $template = $this->getTwig()->load('pageDunContenu.html.twig');
-            echo $template->render(['messagederreur' => 'Vous devez être connecté pour poster un commentaire.']);
-            return;
-        }
-
-        // Récupération et nettoyage des données POST
-        $idTmdbContenu = isset($_POST['idContenu']) ? htmlspecialchars($_POST['idContenu']) : null;
-        $titre = isset($_POST['titre']) ? htmlspecialchars($_POST['titre']) : null;
-        $note = isset($_POST['note']) ? (int)htmlspecialchars($_POST['note']) : null;
-        $commentaireTexte = isset($_POST['commentaire']) ? htmlspecialchars($_POST['commentaire']) : null;
-
-        // Validation des données
-        $message = "";
-        if (!Utilitaires::comprisEntre($titre, 100, 0, "Le titre doit contenir", $message) ||
-            !Utilitaires::comprisEntre($commentaireTexte, 1000, 0, "Le commentaire doit contenir", $message)) {
-            
-            // Redirection vers la page du film avec message d'erreur
-            $controllerContenu = new ControllerContenu($this->getTwig(), $this->getLoader());
-            $_GET['tmdb_id'] = $idTmdbContenu;
-            $controllerContenu->afficherContenu(['messagederreur' => $message]);
-            return;
-        }
-
-        try {
-            // Création de l'objet commentaire
-            $commentaire = new Commentaire(
-                $utilisateur->getId(),
-                $titre,
-                $note,
-                $commentaireTexte,
-                $note >= 3,
-                $idTmdbContenu
-            );
-
-            // Sauvegarde dans la base de données
-            $commentaireDAO = new CommentaireDAO($this->getPdo());
-            $commentaireDAO->createCommentaireContenu($commentaire);
-
-            // Redirection vers la page du contenu avec message de succès
-            $controllerContenu = new ControllerContenu($this->getTwig(), $this->getLoader());
-            $_GET['tmdb_id'] = $idTmdbContenu;
-            $controllerContenu->afficherContenu(['message' => 'Votre commentaire a été ajouté avec succès.']);
-
-        } catch (Exception $e) {
-            // Redirection vers la page du film avec message d'erreur
-            $controllerContenu = new ControllerContenu($this->getTwig(), $this->getLoader());
-            $_GET['tmdb_id'] = $idTmdbContenu;
-            $controllerContenu->afficherContenu(['messagederreur' => $e->getMessage()]);
-        }
+        $this->handleCommentaire(self::TYPE_CONTENU);
     }
 
     /**
      * @brief Crée un nouveau commentaire pour une collection
-     * 
-     * Cette méthode traite la soumission d'un nouveau commentaire pour une collection :
-     * - Récupère les données du formulaire
-     * - Crée un nouvel objet Commentaire
-     * - Sauvegarde le commentaire dans la base de données
-     * - Redirige vers la page de la collection
-     * 
-     * @return void
      */
     public function createCommentaireCollection(): void {
-        // Vérification de la session et désérialisation de l'utilisateur
-        $utilisateur = isset($_SESSION['utilisateur']) ? unserialize($_SESSION['utilisateur']) : null;
+        $this->handleCommentaire(self::TYPE_COLLECTION);
+    }
+
+    /**
+     * @brief Gère la création d'un commentaire
+     */
+    private function handleCommentaire(string $type): void {
+        $config = $this->getConfig($type);
         
+        // Vérification de la session
+        $utilisateur = isset($_SESSION['utilisateur']) ? unserialize($_SESSION['utilisateur']) : null;
         if (!$utilisateur) {
-            $template = $this->getTwig()->load('pageDuneCollection.html.twig');
+            $template = $this->getTwig()->load($config['template']);
             echo $template->render(['messagederreur' => 'Vous devez être connecté pour poster un commentaire.']);
             return;
         }
 
         // Récupération et nettoyage des données POST
-        $idTmdbCollection = isset($_POST['idCollection']) ? htmlspecialchars($_POST['idCollection']) : null;
+        $idTmdb = isset($_POST[$config['idField']]) ? htmlspecialchars($_POST[$config['idField']]) : null;
         $titre = isset($_POST['titre']) ? htmlspecialchars($_POST['titre']) : null;
         $note = isset($_POST['note']) ? (int)htmlspecialchars($_POST['note']) : null;
         $commentaireTexte = isset($_POST['commentaire']) ? htmlspecialchars($_POST['commentaire']) : null;
 
         // Validation des données
         $message = "";
-        if (!Utilitaires::comprisEntre($titre, 100, 0, "Le titre doit contenir", $message) ||
-            !Utilitaires::comprisEntre($commentaireTexte, 1000, 0, "Le commentaire doit contenir", $message)) {
+        if (!Utilitaires::comprisEntre($titre, 100, 3, "Le titre doit contenir", $message) ||
+            !Utilitaires::comprisEntre($commentaireTexte, 1000, 10, "Le commentaire doit contenir", $message)) {
             
-            // Redirection vers la page de la collection avec message d'erreur
-            $controllerCollection = new ControllerCollection($this->getTwig(), $this->getLoader());
-            $_GET['tmdb_id'] = $idTmdbCollection;
-            $controllerCollection->afficherCollection(['messagederreur' => $message]);
+            $this->redirectWithError($type, $idTmdb, $message);
             return;
         }
 
@@ -133,25 +82,66 @@ class ControllerCommentaire extends Controller {
                 $note,
                 $commentaireTexte,
                 $note >= 3,
-                null, // Pas d'ID de contenu, car c'est pour une collection
-                $idTmdbCollection // Utilisation de l'ID de la collection
+                $type === self::TYPE_CONTENU ? $idTmdb : null,
+                $type === self::TYPE_COLLECTION ? $idTmdb : null
             );
 
             // Sauvegarde dans la base de données
             $commentaireDAO = new CommentaireDAO($this->getPdo());
-            $commentaireDAO->createCommentaireCollection($commentaire);
+            $method = $config['createMethod'];
+            $commentaireDAO->$method($commentaire);
 
-            // Redirection vers la page de la collection avec message de succès
-            $controllerCollection = new ControllerCollection($this->getTwig(), $this->getLoader());
-            $_GET['tmdb_id'] = $idTmdbCollection;
-            $controllerCollection->afficherCollection(['message' => 'Votre commentaire a été ajouté avec succès.']);
+            // Redirection avec message de succès
+            $this->redirectWithSuccess($type, $idTmdb);
 
         } catch (Exception $e) {
-            // Redirection vers la page de la collection avec message d'erreur
-            $controllerCollection = new ControllerCollection($this->getTwig(), $this->getLoader());
-            $_GET['tmdb_id'] = $idTmdbCollection;
-            $controllerCollection->afficherCollection(['messagederreur' => $e->getMessage()]);
+            $this->redirectWithError($type, $idTmdb, $e->getMessage());
         }
     }
-}
 
+    /**
+     * @brief Récupère la configuration selon le type de commentaire
+     */
+    private function getConfig(string $type): array {
+        $configs = [
+            self::TYPE_CONTENU => [
+                'template' => 'pageDunContenu.html.twig',
+                'idField' => 'idContenu',
+                'controller' => 'ControllerContenu',
+                'createMethod' => 'createCommentaireContenu',
+                'displayMethod' => 'afficherContenu'
+            ],
+            self::TYPE_COLLECTION => [
+                'template' => 'pageDuneCollection.html.twig',
+                'idField' => 'idCollection',
+                'controller' => 'ControllerCollection',
+                'createMethod' => 'createCommentaireCollection',
+                'displayMethod' => 'afficherCollection'
+            ]
+        ];
+
+        return $configs[$type] ?? throw new Exception("Type de commentaire invalide");
+    }
+
+    /**
+     * @brief Redirige avec un message d'erreur
+     */
+    private function redirectWithError(string $type, string $idTmdb, string $message): void {
+        $config = $this->getConfig($type);
+        $controller = new $config['controller']($this->getTwig(), $this->getLoader());
+        $_GET['tmdb_id'] = $idTmdb;
+        $method = $config['displayMethod'];
+        $controller->$method(['messagederreur' => $message]);
+    }
+
+    /**
+     * @brief Redirige avec un message de succès
+     */
+    private function redirectWithSuccess(string $type, string $idTmdb): void {
+        $config = $this->getConfig($type);
+        $controller = new $config['controller']($this->getTwig(), $this->getLoader());
+        $_GET['tmdb_id'] = $idTmdb;
+        $method = $config['displayMethod'];
+        $controller->$method(['message' => 'Votre commentaire a été ajouté avec succès.']);
+    }
+}
