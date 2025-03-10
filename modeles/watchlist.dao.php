4864deba81ab8f3ef93272483d4d01565b8e801c
Andoni Berho <aberho001@iutbayonne.univ-pau.fr>
2025-03-10 16:01:19 +0100

    Ajout dao watchlist

--- modeles/watchlist.dao.php
+++ modeles/watchlist.dao.php
@@ -83,27 +83,58 @@ class WatchlistDAO
      */
     public function getWatchlistContent(int $watchlistId): array
     {
-        // 1. Récupérer les IDs de contenus liés à la watchlist
-        $sql = "SELECT idContenuTmdb FROM " . DB_PREFIX . "contenirContenu WHERE idWatchlist = :watchlistId ORDER BY rang";
+        $contents = [];
+
+        // 1.1. Récupérer les IDs de contenus liés à la watchlist
+        $sql = "SELECT idContenuTmdb, rang FROM " . DB_PREFIX . "contenirContenu WHERE idWatchlist = :watchlistId ORDER BY rang";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute([':watchlistId' => $watchlistId]);
-        $contentIds = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        $contenusBd = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        if (empty($contentIds)) {
-            return []; // Aucun contenu trouvé pour la watchlist
-        }
-
-        // 2. Utiliser l'API pour récupérer les détails de chaque contenu
+        // 1.2. Utiliser l'API pour récupérer les détails de chaque contenu
         $contenuDAO = new ContenuDAO($this->pdo); // Assurez-vous que cette classe gère l'accès à l'API
-        $contents = [];
-        foreach ($contentIds as $id) {
-            $content = $contenuDAO->getContentFromTMDB($id); // Méthode pour récupérer les détails d'un contenu via l'API
+        foreach ($contenusBd as $contenu) {
+            $content = $contenuDAO->getContentFromTMDB($contenu['idContenuTmdb']); // Méthode pour récupérer les détails d'un contenu via l'API
             if ($content) {
-                $contents[] = $content;
+                $contents[$contenu['rang']] = $content;
             }
         }
 
-        return $contents;
+        // 2.1 Récupérer les IDs de collections liées à la watchlist
+        $sql = "SELECT idCollectionTMDB, rang FROM " . DB_PREFIX . "contenirCollection WHERE idWatchlist = :watchlistId";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':watchlistId' => $watchlistId]);
+        $collectionBd = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // 2.2. Utiliser la classe CollectionDAO pour récupérer les détails de chaque collection
+        $collectionDAO = new CollectionDAO($this->pdo); // Assurez-vous que cette classe gère l'accès à la base de données
+        foreach ($collectionBd as $collection) {
+            $content = $collectionDAO->getCollectionFromTMDB($collection['idCollectionTMDB']); // Méthode pour récupérer les détails d'une collection
+            if ($collection) {
+                $contents[$collection['rang']] = $content;
+            }
+        }
+
+        // 3.1 Récupérer les IDs de séries liées à la watchlist
+        $sql = "SELECT idSerieTMDB, rang FROM " . DB_PREFIX . "contenirSerie WHERE idWatchlist = :watchlistId";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':watchlistId' => $watchlistId]);
+        $serieBd = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // 3.2. Utiliser la classe SerieDAO pour récupérer les détails de chaque série
+        $serieDAO = new SerieDAO($this->pdo); // Assurez-vous que cette classe gère l'accès à la base de données
+        foreach ($serieBd as $serie) {
+            $content = $serieDAO->getSerieFromTMDB($serie['idSerieTMDB']); // Méthode pour récupérer les détails d'une série
+            if ($serie) {
+                $contents[$serie['rang']] = $content;
+            }
+        }
+
+        // Trier les contenus par rang, maintenant clés
+        ksort($contents);
+
+        // Renvoyer seulement les valeurs une fois triées
+        return array_values($contents);
     }
 
     /**
@@ -115,11 +146,8 @@ class WatchlistDAO
      * @return bool Vrai si l'opération a réussi, faux sinon
      */
     public function addContenuToWatchlist(int $watchlistId, int $contenuId): bool {
-        $sql = "SELECT MAX(rang) FROM " . DB_PREFIX . "contenirContenu WHERE idWatchlist = (:watchlistId)";
-        $stmt = $this->pdo->prepare($sql);
-        $max = $stmt->execute([':watchlistId' => $watchlistId]);
-        $rang = ($max == null ? 1 : $max + 1);
-
+        $rang = $this->trouverRang($watchlistId);
+        
         $sql = "INSERT INTO " . DB_PREFIX . "contenirContenu (idWatchlist, idContenuTmdb, rang) VALUES (:watchlistId, :contenuId, :rang)";
         $stmt = $this->pdo->prepare($sql);
         return $stmt->execute([
@@ -127,6 +155,74 @@ class WatchlistDAO
             ':contenuId' => $contenuId,
             ':rang' => $rang
         ]);
+    }
+
+    /**
+     * @brief Méthode pour ajouter une collection à une watchlist
+     * 
+     * @param int $watchlistId Identifiant de la watchlist
+     * @param int $collectionId Identifiant de la collection
+     * 
+     * @return bool Vrai si l'opération a réussi, faux sinon
+     */
+    public function addCollectionToWatchlist(int $watchlistId, int $collectionId): bool {
+        $rang = $this->trouverRang($watchlistId);
+        
+        $sql = "INSERT INTO " . DB_PREFIX . "contenirCollection (idWatchlist, idCollectionTMDB, rang) VALUES (:watchlistId, :collectionId, :rang)";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+            ':watchlistId' => $watchlistId,
+            ':collectionId' => $collectionId,
+            ':rang' => $rang
+        ]);
+    }
+
+    /**
+     * @brief Méthode pour ajouter une série à une watchlist
+     * 
+     * @param int $watchlistId Identifiant de la watchlist
+     * @param int $serieId Identifiant de la série
+     * 
+     * @return bool Vrai si l'opération a réussi, faux sinon
+     */
+    public function addSerieToWatchlist(int $watchlistId, int $serieId): bool {
+        $rang = $this->trouverRang($watchlistId);
+        
+        $sql = "INSERT INTO " . DB_PREFIX . "contenirSerie (idWatchlist, idSerieTMDB, rang) VALUES (:watchlistId, :serieId, :rang)";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+            ':watchlistId' => $watchlistId,
+            ':serieId' => $serieId,
+            ':rang' => $rang
+        ]);
+    }
+
+    public function trouverRang(int $watchlistId): int {
+        // Récupérer le rang maximum dans les contenus
+        $sql = "SELECT MAX(rang) AS maximum FROM " . DB_PREFIX . "contenirContenu WHERE idWatchlist = (:watchlistId)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':watchlistId' => $watchlistId]);
+        $tab = $stmt->fetch();
+        $maxContenu = $tab['maximum'];
+
+        // Récupérer le rang maximum dans les collections
+        $sql = "SELECT MAX(rang) AS maximum FROM " . DB_PREFIX . "contenirCollection WHERE idWatchlist = (:watchlistId)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':watchlistId' => $watchlistId]);
+        $tab = $stmt->fetch();
+        $maxCollec = $tab['maximum'];
+
+        // Récupérer le rang maximum dans les séries
+        $sql = "SELECT MAX(rang) AS maximum FROM " . DB_PREFIX . "contenirSerie WHERE idWatchlist = (:watchlistId)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':watchlistId' => $watchlistId]);
+        $tab = $stmt->fetch();
+        $maxSerie = $tab['maximum'];
+
+        // Déterminer le rang du contenu à ajouter
+        $rang = max($maxContenu, $maxCollec, $maxSerie) + 1;
+
+        return $rang;
     }
 
     /**
@@ -147,13 +243,47 @@ class WatchlistDAO
     }
 
     /**
+     * @brief Méthode pour supprimer une collection d'une watchlist
+     * 
+     * @param int $watchlistId Identifiant de la watchlist
+     * @param int $collectionId Identifiant de la collection
+     * 
+     * @return bool Vrai si l'opération a réussi, faux sinon
+     */
+    public function removeCollectionFromWatchlist(int $watchlistId, int $collectionId): bool {
+        $sql = "DELETE FROM " . DB_PREFIX . "contenirCollection WHERE idWatchlist = :watchlistId AND idCollectionTMDB = :collectionId";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+            ':watchlistId' => $watchlistId,
+            ':collectionId' => $collectionId
+        ]);
+    }
+
+    /**
+     * @brief Méthode pour supprimer une série d'une watchlist
+     * 
+     * @param int $watchlistId Identifiant de la watchlist
+     * @param int $serieId Identifiant de la série
+     * 
+     * @return bool Vrai si l'opération a réussi, faux sinon
+     */
+    public function removeSerieFromWatchlist(int $watchlistId, int $serieId): bool {
+        $sql = "DELETE FROM " . DB_PREFIX . "contenirSerie WHERE idWatchlist = :watchlistId AND idSerieTMDB = :serieId";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+            ':watchlistId' => $watchlistId,
+            ':serieId' => $serieId
+        ]);
+    }
+
+    /**
      * @brief Méthode pour récupérer les watchlists publiques
      * 
      * @return array Tableau d'objets Watchlist
      */
     public function getPublicWatchlists(): array
     {
-        $sql = "SELECT * FROM " . DB_PREFIX . "watchlist WHERE estPublique = true ORDER BY date DESC";
+        $sql = "SELECT * FROM " . DB_PREFIX . "watchlist WHERE estPublique = true";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute();
         return $this->hydrateAll($stmt->fetchAll(PDO::FETCH_ASSOC));
@@ -174,6 +304,21 @@ class WatchlistDAO
             $sqlContenu = "DELETE FROM " . DB_PREFIX . "contenircontenu WHERE idWatchlist = :idWatchlist";
             $stmtContenu = $this->pdo->prepare($sqlContenu);
             $stmtContenu->execute([':idWatchlist' => $idWatchlist]);
+
+            // Suppression des collections associées dans la table de liaison
+            $sqlCollection = "DELETE FROM " . DB_PREFIX . "contenirCollection WHERE idWatchlist = :idWatchlist";
+            $stmtCollection = $this->pdo->prepare($sqlCollection);
+            $stmtCollection->execute([':idWatchlist' => $idWatchlist]);
+
+            // Suppression des séries associées dans la table de liaison
+            $sqlSerie = "DELETE FROM " . DB_PREFIX . "contenirSerie WHERE idWatchlist = :idWatchlist";
+            $stmtSerie = $this->pdo->prepare($sqlSerie);
+            $stmtSerie->execute([':idWatchlist' => $idWatchlist]);
+
+            // Suppression des partages associés dans la table de liaison
+            $sqlPartage = "DELETE FROM " . DB_PREFIX . "partager WHERE idWatchlist = :idWatchlist";
+            $stmtPartage = $this->pdo->prepare($sqlPartage);
+            $stmtPartage->execute([':idWatchlist' => $idWatchlist]);
             
             // Suppression de la watchlist
             $sqlWatchlist = "DELETE FROM " . DB_PREFIX . "watchlist WHERE idWatchlist = :idWatchlist";
@@ -210,6 +355,9 @@ class WatchlistDAO
         // Récupérer et assigner les contenus
         $watchlist->setContenus($this->getWatchlistContent($data['idWatchlist']));
 
+        // Réaliser et assigner les partages
+
+
         return $watchlist;
     }
 
@@ -245,6 +393,174 @@ class WatchlistDAO
             ':contenuId' => $contenuId
         ]);
         return intval($stmt->fetchColumn()) > 0;
-    }   
+    }
+
+    /**
+     * @brief Méthode pour vérifier si une collection est dans une watchlist
+     * 
+     * @param int $watchlistId Identifiant de la watchlist
+     * @param int $collectionId Identifiant de la collection
+     * 
+     * @return bool Vrai si la collection est dans la watchlist, faux sinon
+     */
+    public function isCollectionInWatchlist(int $watchlistId, int $collectionId): bool
+    {
+        $sql = "SELECT COUNT(*) FROM " . DB_PREFIX . "contenirCollection WHERE idWatchlist = :watchlistId AND idCollectionTMDB = :collectionId";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([
+            ':watchlistId' => $watchlistId,
+            ':collectionId' => $collectionId
+        ]);
+        return intval($stmt->fetchColumn()) > 0;
+    }
+
+    /**
+     * @brief Méthode pour vérifier si une série est dans une watchlist
+     * 
+     * @param int $watchlistId Identifiant de la watchlist
+     * @param int $serieId Identifiant de la série
+     * 
+     * @return bool Vrai si la série est dans la watchlist, faux sinon
+     */
+    public function isSerieInWatchlist(int $watchlistId, int $serieId): bool
+    {
+        $sql = "SELECT COUNT(*) FROM " . DB_PREFIX . "contenirSerie WHERE idWatchlist = :watchlistId AND idSerieTMDB = :serieId";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([
+            ':watchlistId' => $watchlistId,
+            ':serieId' => $serieId
+        ]);
+        return intval($stmt->fetchColumn()) > 0;
+    }
+    
+    /**
+     * @brief Méthode pour récupérer les watchlists publiques
+     * 
+     * @return array Tableau d'objets Watchlist
+     */
+    public function getWatchlistPublic(): array
+    {
+        $sql = "SELECT * FROM " . DB_PREFIX . "watchlist WHERE estPublique = 1";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $this->hydrateAll($stmt->fetchAll(PDO::FETCH_ASSOC));
+    }
+
+    /**
+     * @brief Méthode pour récupérer les partages d'une watchlist
+     * 
+     * @param int $idWatchlist Identifiant de la watchlist
+     * 
+     * @return array Tableau d'objets (int idWatchlist, string idUtilisateurC, string idUtilisateurP)
+     */
+    public function getWatchlistPartages(int $idWatchlist): array
+    {
+        $sql = "SELECT * FROM " . DB_PREFIX . "partager WHERE idWatchlist = :idWatchlist";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':idWatchlist' => $idWatchlist]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @brief Méthode pour partager une watchlist
+     * 
+     * @param int $idWatchlist Identifiant de la watchlist
+     * @param string $idUtilisateurC Identifiant de l'utilisateur créateur
+     * @param string $idUtilisateurP Identifiant de l'utilisateur à partager avec
+     * 
+     * @return bool Vrai si l'opération a réussi, faux sinon
+     */
+    public function partagerWatchlist(int $idWatchlist, string $idUtilisateurC, string $idUtilisateurP): bool
+    {
+        $sql = "INSERT INTO " . DB_PREFIX . "partager (idWatchlist, idUtilisateurC, idUtilisateurP) VALUES (:idWatchlist, :idUtilisateurC, :idUtilisateurP)";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+            ':idWatchlist' => $idWatchlist,
+            ':idUtilisateurC' => $idUtilisateurC,
+            ':idUtilisateurP' => $idUtilisateurP
+        ]);
+    }
+
+    /**
+     * @brief Méthode pour retirer un partage d'une watchlist
+     * 
+     * @param int $idWatchlist Identifiant de la watchlist
+     * @param string $idUtilisateurP Identifiant de l'utilisateur à retirer
+     * 
+     * @return bool Vrai si l'opération a réussi, faux sinon
+     */
+    public function retirerPartageWatchlist(int $idWatchlist, string $idUtilisateurP): bool
+    {
+        $sql = "DELETE FROM " . DB_PREFIX . "partager WHERE idWatchlist = :idWatchlist AND idUtilisateurP = :idUtilisateurP";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+            ':idWatchlist' => $idWatchlist,
+            ':idUtilisateurP' => $idUtilisateurP
+        ]);
+    }
+
+    /**
+     * @brief Méthode pour vérifier si une watchlist est partagée avec un utilisateur
+     * 
+     * @param int $idWatchlist Identifiant de la watchlist
+     * @param string $idUtilisateurP Identifiant de l'utilisateur
+     * 
+     * @return bool Vrai si la watchlist est partagée avec l'utilisateur, faux sinon
+     */
+    public function isWatchlistShared(int $idWatchlist, string $idUtilisateurP): bool
+    {
+        $sql = "SELECT COUNT(*) FROM " . DB_PREFIX . "partager WHERE idWatchlist = :idWatchlist AND idUtilisateurP = :idUtilisateurP";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([
+            ':idWatchlist' => $idWatchlist,
+            ':idUtilisateurP' => $idUtilisateurP
+        ]);
+        return intval($stmt->fetchColumn()) > 0;
+    }
+
+    /**
+     * @brief Méthode pour récupérer les watchlists partagées avec un utilisateur
+     * 
+     * @param string $idUtilisateur Identifiant de l'utilisateur
+     * 
+     * @return array Tableau d'objets Watchlist
+     */
+    public function getWatchlistsSharedWithUser(string $idUtilisateur): array
+    {
+        $sql = "SELECT * FROM " . DB_PREFIX . "partager WHERE idUtilisateurP = :idUtilisateur";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':idUtilisateur' => $idUtilisateur]);
+        $partages = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $watchlists = [];
+        foreach ($partages as $partage) {
+            $watchlist = $this->findById($partage['idWatchlist']);
+            if ($watchlist) {
+                $watchlists[] = $watchlist;
+            }
+        }
+
+        return $watchlists;
+    }
+
+    /**
+     * @brief Méthode pour récupérer une watchlist par son ID
+     * 
+     * @param int $id Identifiant de la watchlist
+     * @return Watchlist|null Objet Watchlist ou null si non trouvé
+     */
+    public function findById(int $id): ?Watchlist
+    {
+        $sql = "SELECT * FROM " . DB_PREFIX . "watchlist WHERE idWatchlist = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':id' => $id]);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $row = $stmt->fetch();
+        if ($row) {
+            return $this->hydrate($row);
+        }
+        return null;
+    }
+
 }
 
